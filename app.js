7832e73b38c2418ba4ce291a53e26015a113f6df
Vsevolod <vsevolodehe@mail.ru>
2022-06-13 15:23:26 +0500

    work in progress

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-let f = debounce(console.log, 1000);
-
-f(1); // выполняется немедленно
-f(2); // проигнорирован
-
-setTimeout( () => f(3), 100); // проигнорирован (прошло только 100 мс)
-setTimeout( () => f(4), 1100); // выполняется
-setTimeout( () => f(5), 1500); 
-
-
-function debounce(func, delay){
-  let flag = true
-  return function(){
-      if (flag){
-        func.apply(this, arguments)
-        flag = false
-        setTimeout(() => {flag = true}, delay)        
-      }
-  }
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
 }
+
+let user = {
+  name: 'John',
+
+  login(result) {
+    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+  }
+};
+
+askPassword(() => user.login.bind(user)(true), () => user.login.bind(user)(false)); // ?
+
+
+
+
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", '');
+//   if (password == "rockstar") ok();
+//   else fail();
+// }
+
+// let user = {
+//   name: 'Вася',
+
+//   loginOk() {
+//     alert(`${this.name} logged in`);
+//   },
+
+//   loginFail() {
+//     alert(`${this.name} failed to log in`);
+//   },
+
+// };
+
+// askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+
+// let f = debounce(console.log, 1000);
+
+// f(1); // выполняется немедленно
+// f(2); // проигнорирован
+
+// setTimeout( () => f(3), 100); // проигнорирован (прошло только 100 мс)
+// setTimeout( () => f(4), 1100); // выполняется
+// setTimeout( () => f(5), 1500); 
+
+
+// function debounce(func, delay){
+//   let flag = true
+//   return function(){
+//       if (flag){
+//         func.apply(this, arguments)
+//         flag = false
+//         setTimeout(() => {flag = true}, delay)        
+//       }
+//   }
+// }
