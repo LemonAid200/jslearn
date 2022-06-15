6a1b707f8c0d492d9e5d7d0da7ba731fccb8236d
Vsevolod <vsevolodehe@mail.ru>
2022-06-16 03:02:33 +0500

    done with classes

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
-function askPassword(ok, fail) {
-  let password = prompt("Password?", '');
-  if (password == "rockstar") ok();
-  else fail();
-}
 
-let user = {
-  name: 'John',
 
-  login(result) {
-    alert( this.name + (result ? ' logged in' : ' failed to log in') );
-  }
-};
 
-askPassword(() => user.login.bind(user)(true), () => user.login.bind(user)(false)); // ?
+
+
+
+
+
+// class FormatError extends SyntaxError{
+//     constructor(message){
+//         super()
+//         this.message = message
+//         this.name = this.constructor.name
+//     }
+// }
+
+
+
+
+// let err = new FormatError("ошибка форматирования");
+
+// console.log( err.message ); // ошибка форматирования
+// console.log( err.name ); // FormatError
+// console.log( err.stack ); // stack
+// console.log( err instanceof FormatError ); // true
+// console.log( err instanceof SyntaxError ); // true (потому что наследует от SyntaxError)
+
+
+
+
+
+
+
+
+
+
+
+
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?", '');
+//   if (password == "rockstar") ok();
+//   else fail();
+// }
+
+// let user = {
+//   name: 'John',
+
+//   login(result) {
+//     alert( this.name + (result ? ' logged in' : ' failed to log in') );
+//   }
+// };
+
+// askPassword(() => user.login.bind(user)(true), () => user.login.bind(user)(false)); // ?
 
 
 
